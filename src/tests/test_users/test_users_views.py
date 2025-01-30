93321f46a0ddbc2ca3d9c74c6f9c4b27b949c40b
Mike Torres <michael@aqwire.co>
2025-01-30 17:00:07 +0700

    add test to get_user_detail

--- src/tests/test_users/test_users_views.py
+++ src/tests/test_users/test_users_views.py
@@ -1,7 +1,5 @@
 from django.urls import reverse
 
-from django_admin.constants import DASHBOARD_URL_PREFIX
-
 
 def test_get_all_users_non_admin(api_client, non_admin_token):
     client = api_client()
@@ -26,3 +24,28 @@ def test_get_all_users_admin(api_client, superuser_token, limited_admin_token):
     
     assert 'users' in data
     assert len(data.get('users')) == 2
+
+def test_get_user_detail_non_admin(api_client, non_admin_token, limited_admin):
+    client = api_client()
+
+    response = client.get(
+        reverse('get_user_detail', kwargs={'uid': limited_admin.uid}),
+        format='json',
+        **{'HTTP_AUTHORIZATION': f'Bearer {non_admin_token}'}
+    )
+    assert response.status_code == 401
+
+def test_get_user_detail_admin(api_client, superuser_token, limited_admin):
+    client = api_client()
+
+    response = client.get(
+        reverse('get_user_detail', kwargs={'uid': limited_admin.uid}), 
+        format='json',
+        **{'HTTP_AUTHORIZATION': f'Bearer {superuser_token}'}
+    )
+    assert response.status_code == 200
+    data = response.data
+    
+    assert 'user' in data
+    assert data.get('user').get('uid') == limited_admin.uid
+    
