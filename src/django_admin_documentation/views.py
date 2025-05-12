d01bbb582b8b9f25ccbc1e3756f1835ad55332c5
Mike Torres <michael@aqwire.co>
2025-05-12 20:01:25 +0700

    add permissions to get model docs view

--- src/django_admin_documentation/views.py
+++ src/django_admin_documentation/views.py
@@ -1,10 +1,11 @@
 from django.core.cache import cache
 from drf_spectacular.utils import OpenApiResponse, extend_schema
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 from django_admin.data_transform import transform_dict_to_camel_case
+from django_admin.permissions import IsActiveAdminUser
 
 from .docs import GET_MODEL_DOCS_DOC
 from .models import ModelDocumentation
@@ -20,6 +21,7 @@ from .serializers import AdminModelDocumentationSerializer
     }
 )
 @api_view(['GET'])
+@permission_classes([IsActiveAdminUser])
 def get_model_docs(request):
     cache_key = ModelDocumentation.CACHE_KEY_PREFIX
     cached_data = cache.get(cache_key)
